e82ffbaa21e959acaa6b35c85b4bea115d3cad4d
Roman-CCFI <92735744+Roman-CCFI@users.noreply.github.com>
2021-10-19 13:57:30 -0400

    All parts working
    Installation still looks ugly as sin, but the installation process works.

--- librespeed-setup.sh
+++ librespeed-setup.sh
@@ -1,45 +1,50 @@
 #!/bin/sh
 #The purpose of this script is to assist in expiditing the installation of LibreSpeed to external entities.
 echo "Beginning the installation process"
-echo "what is the username that will be responisble for the installation of all dependancies and the application? Please note this user must have ROOT privileges."
-read uname
-echo "what is the username's password? This will only temporarily be stored and will be removed from memory once installation completes."
-read -s upass
-sudo su
-echo $upass
+sleep 2s
 echo "Installing prequeqets..."
-apt install -y apahce2 php php-{pear,cgi,common,curl,mbstring,gd,mysql,bcmath,imap,json,xml,snmp,fpm,zip} mysql-server phpmyadmin
+sleep 2s
+apt install -y apache2 php php-pear php-cgi php-common php-curl php-mbstring php-gd php-mysql php-bcmath php-imap php-json php-xml php-snmp php-fpm php-zip mysql-server
 echo "cloning LibreSpeed Repository"
+sleep 2s
 git clone https://github.com/adolfintel/speedtest.git
-cp -R ~/speedtest/backend/ ~/speedtest/results/ example-singleServer-pretty.html *.js /var/www/html/
-mv /var/www/html/example-singleServer-pretty index.html
+rm /var/www/html/index.html
+cp -R speedtest/backend/ speedtest/results/ speedtest/example-singleServer-pretty.html speedtest/*.js /var/www/html/
+mv /var/www/html/example-singleServer-pretty.html /var/www/html/index.html
 chown -R www-data /var/www/html/
-mysql -u root
-cp ~/speedtest/example-singleServer-full.html /var/www/html/index.html
+echo "Configuring MySQL"
+sleep 2s
 #MySQL Configuration
-alter user 'root'@'localhost' identified with mysql_native_password by 'PASSWORD';
+mysql -u root << EOF
+alter user 'root'@'localhost' identified with mysql_native_password by 'P@ssw0rd';
 flush privileges;
-Create database;
 CREATE DATABASE speedtest;
-mysql -u root -p speedtest < ~/speedtest/results/telemetry_mysql.sql
+EOF
+mysql -u root -p speedtest < speedtest/results/telemetry_mysql.sql
 echo "Now updating telemetry settings."
+sleep 2s
 echo "Please enter the status page password"
-read -s statpass
+stty -echo
+read statpass
+stty echo
 echo "If set to true, test IDs will be obfuscated to prevent users from guessing URLs of other tests. Please enter True or False."
 read obfus
 echo "If set to true, IP addresses will be redacted from IP and ISP info fields, as well as the log. Please enter True or False."
 read ipfield
-echo "Please enter MySQL username"
-read msqlunam
-echo "Please enter MySQL Password"
-read -s msqlpass
-hname = localhost
-dbname = speedtest
 
-sed -i -e "s/\(stats_password = '\).*/\1$statpass/" \
--e "s/\(enable_id_obfuscation = \).*/\1$obfus/" \
--e "s/\(redact_ip_addresses = \).*/\1$ipfield/" \
--e "s/\(MySql_username = '\).*/\1$msqlunam/" \
--e "s/\(MySql_password = '\).*/\1$msqlpass/" \
--e "s/\(MySql_hostname = '\).*/\1$hname/" \
--e "s/\(MySql_databasename = '\).*/\1$dbname/" /var/www/html/results/telemetry_settings.php
+sed -i -e "s/\(stats_password = '\).*/\1$statpass';/" \
+-e "s/\(enable_id_obfuscation = \).*/\1$obfus;/" \
+-e "s/\(redact_ip_addresses = \).*/\1$ipfield;/" \
+-e "s/\(MySql_username = '\).*/\1root';/" \
+-e "s/\(MySql_password = '\).*/\1P@ssw0rd';/" \
+-e "s/\(MySql_hostname = '\).*/\1localhost';/" \
+-e "s/\(MySql_databasename = '\).*/\1speedtest';/" /var/www/html/results/telemetry_settings.php
+
+cp speedtest/example-singleServer-full.html /var/www/html/index.html
+chown -R www-data /var/www/html/
+
+echo "The password for your status page is: "$statpass ". you can access it at http://localhost/results/stats.php"
+sleep 5s
+echo "Your temporary password for MySQL root user is 'P@ssw0rd', this can be changed by running the following command in MySQL: alter user 'root'@'localhost' identified with mysql_native_password by 'PASSWORD'; "
+sleep 2s
+echo "Please ensure that you update this password in /var/www/html/results/telemetry_settings.php. This is also where you can change the password for the status page."
